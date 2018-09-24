394be2df05a1418c724afce8c71613c4aba1e947
0nePoint0ne <40418659+0nePoint0ne@users.noreply.github.com>
2018-09-25 07:37:35 +0900

    basics done
    I create import and export methods for program

---
+++ Braille-Code-To-Text.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+dic = {' ': '000000','b': '101000', 'a': '100000'}
+
+
+def importData():
+	data = pd.read_csv("data.csv", header=None)
+	values = data[0].to_string(index = False).split()
+	codes = data[1].to_string(index = False).split()
+	
+	
+	for i in range(len(values)):
+		temp1 = values[i-1]
+		temp2 = codes[i-1]
+		dic.update({temp1: temp2})
+	#print(values)
+	#dic[]
+	print(dic)
+	print(dic['a'])
+def exportData():
+	codes = []
+	values = []
+	
+	for i in dic:
+		codes.append(dic[i])
+		values.append(i)
+	
+	df = pd.DataFrame(codes, values)
+	
+	df.to_csv("data.csv",header=None)
+	
+def learnCharacter(text, binaryText):
+	
+
+	
+
+def main():
+	importData()
+	
+	exportData()
+main()
